97e26bb937e4d3d41e53b7f933de389272103ccb
Emmanuel Buckshi <adventurist@gmail.com>
2023-12-28 18:20:23 -0500

    cleanup

--- index.js
+++ index.js
@@ -3,35 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 4958; // You can use any port you prefer
+const PORT = 4958;
 
-// Read the JSON file
 const filePath = path.join(__dirname, 'quotes.json');
 const quotesData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-// Define a route to handle GET requests
-app.get('/', (req, res) => {
-  const name = req.query.name;
+//-------------------------------------------------
+app.get('/', (req, res) =>
+{
+  const name = req.query.name.toLowerCase();
 
-  // Check if the name parameter is provided
-  if (!name) {
+  if (!name)
     return res.status(400).json({ error: 'Name parameter is required.' });
-  }
 
-  // Check if the provided name exists in the quotes data
-  if (!quotesData[name]) {
+  if (!quotesData[name])
     return res.status(404).send("Fuck you asshole. - Arnold");
-  }
 
-  // Get a random quote for the specified name
   const quotes = quotesData[name];
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   res.send(randomQuote)
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
+})
+//-------------------------------------------------
+app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`)})
