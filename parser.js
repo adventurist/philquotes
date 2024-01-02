ee7193cdedbb9bd5ae44ee32fe5995c08cb66289
Emmanuel Buckshi <adventurist@gmail.com>
2024-01-02 13:27:07 -0500

    updated

--- parser.js
+++ parser.js
@@ -1,17 +1,14 @@
-const path = require('path')
-const fs = require('fs')
-const file = path.resolve(process.argv[2])
-const qdata = path.resolve(__dirname, "quotes.json")
-const name = process.argv[3]
-const input = fs.readFileSync(file).toString()
-const db = JSON.parse(fs.readFileSync(qdata).toString())
-const quotes = []
-let y = 0
-const raw = input.split("\n\n")
-for (const text of raw)
+const path   = require('path')
+const fs     = require('fs')
+const file   = path.resolve(process.argv[2])
+const qdata  = path.resolve(__dirname, "quotes.json")
+const name   = process.argv[3]
+const input  = fs.readFileSync(file).toString()
+const db     = JSON.parse(fs.readFileSync(qdata).toString())
+const quotes = (name in db) ? db[name] : []
+
+for (const text of input.split("\n\n"))
   quotes.push(text)
-
-
 
 db[name] = quotes
 fs.writeFileSync(qdata, JSON.stringify(db))
