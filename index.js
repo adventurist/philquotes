159079828644ed27ea5bd1022a1fbd966f7c537d
Emmanuel Buckshi <adventurist@gmail.com>
2024-01-02 10:57:03 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-const PORT = 4958;
-
-const filePath = path.join(__dirname, 'quotes.json');
-const quotesData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-const names = [ 'hegel', 'marx', 'random', 'lenin', 'mao' ]
+const express = require('express')
+const fs      = require('fs')
+const path    = require('path')
+const app     = express()
+const PORT    = 4958
+const file    = path.join(__dirname, 'quotes.json');
+const data    = JSON.parse(fs.readFileSync(file, 'utf8'));
+const names   = [ 'hegel', 'marx', 'random', 'lenin', 'mao', 'trans', 'marcuse' ]
 //-------------------------------------------------
 app.get('/', (req, res) =>
 {
@@ -16,13 +14,13 @@ app.get('/', (req, res) =>
   if (!name)
     return res.status(400).json({ error: 'Name parameter is required.' });
 
-  if (!quotesData[name])
+  if (!data[name])
     return res.status(404).send("Fuck you asshole. - Arnold");
 
-  const quotes = quotesData[name];
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  const quotes = data[name];
+  const text   = quotes[Math.floor(Math.random() * quotes.length)];
 
-  res.send(randomQuote)
+  res.json({name, text})
 })
 //-------------------------------------------------
 app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`)})
